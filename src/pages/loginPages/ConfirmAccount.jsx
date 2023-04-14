f605001db1b919d0d360130d6f070fe2c0c4b18e
Bemart93 <bemart93@gmail.com>
2023-04-14 19:53:41 +0200

    FEAT: creating confirm page

--- src/pages/loginPages/ConfirmAccount.jsx
+++ src/pages/loginPages/ConfirmAccount.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const ConfirmAccount = () => {
   return (
-    <div>
-      ConfirmAccount
-    </div>
+    <>
+    <h1 className="text-gray-600 text-3xl font-bold tracking-wide text-center">
+    <span className="text-slate-700 uppercase">¡Listo!<br></br></span> 
+    En breves te llegará un email para confirmar tu contraseña
+      
+    </h1>
+    </>
   )
 }
 
