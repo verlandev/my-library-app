92d7c3ddccb4042ae41843a45bb9a2f91b8f167d
Bemart93 <bemart93@gmail.com>
2023-05-02 22:32:55 +0200

    STYLE: dark theme ForgotPassword Page

--- src/pages/loginPages/ForgotPassword.jsx
+++ src/pages/loginPages/ForgotPassword.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from './../../assets/pluma.png'
+
 
 const ForgotPassword = () => {
   return (
     <>
-      <h1 className="text-gray-600 text-3xl font-bold tracking-wide text-center">
-        Desde aquí puedes recuperar tu contraseña y acceder a la {""}
-        <span className="text-slate-700">librería</span>
+      <img
+        src={logo}
+        className="invert w-1/5 mx-auto"
+       />
+      <h1 className="text-gray-500 text-3xl font-bold tracking-wide text-center">
+        SHELFIE <br></br>
+        <span className="text-slate-400">Recupera tu cuenta</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+      <form className="my-10 bg-slate-700 shadow rounded-lg px-10 pb-10 pt-5">
         <div className="my-5">
           <label
-            className="uppercase text-gray-600 block text-lg font-bold tracking-wide"
+            className="uppercase text-gray-400 block text-lg font-bold tracking-wide"
             htmlFor="email"
           >
             Email
@@ -21,25 +27,25 @@ const ForgotPassword = () => {
             id="email"
             type="email"
             placeholder="Escribe tu email"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50 outline-sky-200"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-400 outline-sky-200 placeholder:text-slate-200"
           />
         </div>
 
         <input
           id="submit"
-          value="Recuperar contraseña"
-          className=" w-full py-2 bg-sky-700 rounded text-center uppercase text-white tracking-wide font-medium mt-5 cursor-pointer hover:bg-sky-900 transition-colors duration-300 ease-linear focus:outline-none"
+          value="Recupera tu contraseña"
+          className=" block w-2/3 mx-auto py-2 bg-cyan-500 rounded-xl text-center uppercase text-white tracking-wide font-medium mt-10 cursor-pointer hover:bg-cyan-600 transition-colors duration-300 ease-linear focus:outline-none"
         />
-      </form>
-
-      <nav className="lg:flex lg:justify-between">
+        <p className="text-center py-3 text-gray-50 ">
+          O
+        </p>
         <Link
-          to="register"
-          className="block text-center text-slate-500 text-sm hover:text-slate-800 transition transition-duration-300 ease-lineal tracking-wide"
+          to="/register"
+          className="block w-2/3 mx-auto py-2 border rounded-xl text-center uppercase text-cyan-400 tracking-wide font-medium cursor-pointer hover:bg-cyan-600 hover:text-white hover:border-cyan-600 transition-colors duration-300 ease-linear focus:outline-none "
         >
-          ¿No tienes una cuenta? Regístrate
+          Regístrate
         </Link>
-      </nav>
+      </form>
     </>
   );
 };
