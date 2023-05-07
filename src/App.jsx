b655b8baa3bd038613924ab4cbf2724c0a998715
Bemart93 <bemart93@gmail.com>
2023-05-07 17:25:59 +0200

    FEAT: creating authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AuthLayout from "./layouts/AuthLayout"
-import Login from "./pages/loginPages/Login"
-import Register from "./pages/loginPages/Register"
-import ForgotPassword from "./pages/loginPages/ForgotPassword"
-import NewPassword from "./pages/loginPages/NewPassword"
-import ConfirmAccount from "./pages/loginPages/ConfirmAccount"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/loginPages/Login";
+import Register from "./pages/loginPages/Register";
+import ForgotPassword from "./pages/loginPages/ForgotPassword";
+import NewPassword from "./pages/loginPages/NewPassword";
+import ConfirmAccount from "./pages/loginPages/ConfirmAccount";
 
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/forgot-password/:token" element={<NewPassword />} />
-          <Route path="/confirm/:id" element={<ConfirmAccount />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/forgot-password/:token" element={<NewPassword />} />
+            <Route path="/confirm/:id" element={<ConfirmAccount />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect, createContext } from "react";
+
+const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+    return(
+        <AuthContext.Provider
+            value={{
+
+            }}
+        >
+        {children}
+        </AuthContext.Provider>
+    )
+
+}
+
+export {
+    AuthProvider
+}
