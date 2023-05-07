6acb42afd3b38959f2e408c3c393d5549e0412b7
Bemart93 <bemart93@gmail.com>
2023-05-07 18:23:39 +0200

    FEAT: inspect token for user authentication

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,39 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
 
         const [auth, setAuth] = useState({})
+
+        useEffect(() => {
+            const authenticateUser = async () => {
+                const token = localStorage.getItem('token')
+                if(!token){
+                    return
+                }
+
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+
+                try {
+                    const { data } = await axios(`${import.meta.env.VITE_BACKEND_URL}/users/profile`, config)
+                    setAuth(data)
+
+                } catch (error) {
+                    console.log(error)
+                    
+                }
+            }
+            authenticateUser()
+            
+        }, [])
+
 
     return(
         <AuthContext.Provider
