b0159d2e409200029a17f6c2869652a3edd31f80
Bemart93 <bemart93@gmail.com>
2023-05-15 12:37:37 +0200

    STYLE: iconMenu and search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,23 @@ import React from "react";
 import logoMenu from "../assets/menu.png";
 import logo from "../assets/pluma.png";
 
-
 const Header = () => {
   return (
-    <header className="flex md:flex-row md:justify-center m-auto w-1/2 py-10">
-        <div className="w-full flex md:flex-col justify-center">
-            <img
-                src={logo}
-                className="invert w-20 mb-2 mx-auto"
-            />
-            <h2 className="text-gray-200 text-2xl mb-10 text-center font-bold tracking-widest">
-                SHELFIE
-            </h2>
-            <input
-                type="search"
-                placeholder="¿Qué libro quieres?"
-                className="w-full rounded-md outline-sky-400 px-2 placeholder:text-slate-400 placeholder:text-center"
-            />
-        </div>
-        <div>
-            
-        </div>
+    <header className=" m-auto me-0 w-full py-10">
+      <div className="flex md:flex-row md:justify-center">
+        <img src={logoMenu} className="w-10 invert ml-10 cursor-pointer" />
+        <input
+          type="search"
+          placeholder="Buscar"
+          className="w-1/4 m-auto rounded-lg px-2 text-center placeholder:tracking-widest outline-sky-400"
+        />
+        <button
+          type="button"
+          className="border w-40 text-gray-100 mr-10 rounded-lg tracking-wider hover:border-cyan-600 hover:bg-cyan-600 transition-colors duration-300 ease-linear focus:outline-none"
+        >
+          Cerrar sesión
+        </button>
+      </div>
     </header>
   );
 };
